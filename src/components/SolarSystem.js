e332e643b15f63b13d5719c167c2adbf24f578a0
Ivens <ivensarroxellas@hotmail.com>
2022-07-20 14:57:48 -0300

    correção do header e criação do solarsystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div>SolarSystem</div>
+    );
+  }
+}
+
+export default SolarSystem;
